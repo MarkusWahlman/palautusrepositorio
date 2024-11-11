43dffe5ded5189950ee5cc06db149ae7d74df3b8
Markus Wahlman <markus.wahlman@helsinki.fi>
2024-11-11 23:44:54 +0200

    Add all data to Player

--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,6 +1,18 @@
 class Player:
-    def __init__(self, dict):
-        self.name = dict['name']
+    def __init__(self, player_data):
+        self.name = player_data['name']
+        self.nationality = player_data['nationality']
+        self.assists = player_data['assists']
+        self.goals = player_data['goals']
+        self.team = player_data['team']
+        self.games = player_data['games']
+        self.id = player_data['id']
     
     def __str__(self):
-        return self.name
+        return (f"Name: {self.name}\n"
+                f"Nationality: {self.nationality}\n"
+                f"Team: {self.team}\n"
+                f"Games Played: {self.games}\n"
+                f"Goals: {self.goals}\n"
+                f"Assists: {self.assists}\n"
+                f"Player ID: {self.id}")
