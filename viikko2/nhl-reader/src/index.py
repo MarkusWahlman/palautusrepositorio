a243c73e7042e959a5330c290edfdbec266bc733
Markus Wahlman <markus.wahlman@helsinki.fi>
2024-11-12 00:10:42 +0200

    Refactor code

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,25 +1,11 @@
-import requests
-from player import Player
-
-def check_player_finnish(player: Player):
-    if player.nationality == "FIN":
-        return True
-    return False
+from player import PlayerReader, PlayerStats
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
-    response = requests.get(url, timeout=5).json()
-
-    players = []
-
-    for player_dict in response:
-        player = Player(player_dict)
-        players.append(player)
-
-    finnish_players = filter(check_player_finnish, players)
-    sorted_finnish_players = sorted(finnish_players, key=lambda player: player.total_points, reverse=True)
-
-    for player in sorted_finnish_players:
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+    players = stats.top_scorers_by_nationality("FIN")
+    for player in players:
         print(player)
 
 if __name__ == "__main__":
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,3 +1,6 @@
+import requests
+
+
 class Player:
     def __init__(self, player_data):
         self.name = player_data['name']
@@ -11,3 +14,19 @@ class Player:
     
     def __str__(self):
             return f"{self.name:<20} {self.team}  {self.goals} + {self.assists} = {self.total_points}"
+
+class PlayerReader:
+    def __init__(self, url: str):
+        self.url = url
+
+    def get_players(self):
+        response = requests.get(self.url, timeout=5).json()
+        return [Player(player_data) for player_data in response]
+
+class PlayerStats:
+    def __init__(self, reader: PlayerReader):
+        self.players = reader.get_players()
+
+    def top_scorers_by_nationality(self, nationality: str):
+        filtered_players = filter(lambda player: player.nationality == nationality, self.players)
+        return sorted(filtered_players, key=lambda player: player.total_points, reverse=True)
