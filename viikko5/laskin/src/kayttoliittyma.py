abfe510ed98fa3d27bdef8030c52ae98df9a3fb1
Markus Wahlman <markus.wahlman@helsinki.fi>
2024-12-02 13:48:12 +0200

    Refactor laskin

--- viikko5/laskin/src/kayttoliittyma.py
+++ viikko5/laskin/src/kayttoliittyma.py
@@ -1,18 +1,61 @@
+from abc import abstractmethod
 from enum import Enum
 from tkinter import ttk, constants, StringVar
 
 
-class Komento(Enum):
+class CommandType(Enum):
     SUMMA = 1
     EROTUS = 2
     NOLLAUS = 3
     KUMOA = 4
 
+class Command:
+    def __init__(self, sovelluslogiikka, lue_syote):
+        self._lue_syote = lue_syote
+        self._sovelluslogiikka = sovelluslogiikka
+
+    @abstractmethod
+    def suorita(self):
+        pass
+
+class Summa(Command):
+    def suorita(self):
+        try:
+            arvo = int(self._lue_syote())
+            self._sovelluslogiikka.plus(arvo)
+        except Exception:
+            pass
+
+class Erotus(Command):
+    def suorita(self):
+        try:
+            arvo = int(self._lue_syote())
+            self._sovelluslogiikka.miinus(arvo)
+        except Exception:
+            pass
+
+class Nollaus(Command):
+    def suorita(self):
+        self._sovelluslogiikka.nollaa()
+
+class Kumoa(Command):
+    def suorita(self):
+        self._sovelluslogiikka.kumoa()
 
 class Kayttoliittyma:
     def __init__(self, sovelluslogiikka, root):
         self._sovelluslogiikka = sovelluslogiikka
         self._root = root
+
+        self._komennot = {
+            CommandType.SUMMA: Summa(sovelluslogiikka, self._lue_syote),
+            CommandType.EROTUS: Erotus(sovelluslogiikka, self._lue_syote),
+            CommandType.NOLLAUS: Nollaus(sovelluslogiikka, self._lue_syote),
+            CommandType.KUMOA: Kumoa(sovelluslogiikka, self._lue_syote) # ei ehkä tarvita täällä...
+        }
+
+    def _lue_syote(self):
+        return self._syote_kentta.get()
 
     def kaynnista(self):
         self._arvo_var = StringVar()
@@ -24,27 +67,27 @@ class Kayttoliittyma:
         summa_painike = ttk.Button(
             master=self._root,
             text="Summa",
-            command=lambda: self._suorita_komento(Komento.SUMMA)
+            command=lambda: self._suorita_komento(CommandType.SUMMA)
         )
 
         erotus_painike = ttk.Button(
             master=self._root,
             text="Erotus",
-            command=lambda: self._suorita_komento(Komento.EROTUS)
+            command=lambda: self._suorita_komento(CommandType.EROTUS)
         )
 
         self._nollaus_painike = ttk.Button(
             master=self._root,
             text="Nollaus",
             state=constants.DISABLED,
-            command=lambda: self._suorita_komento(Komento.NOLLAUS)
+            command=lambda: self._suorita_komento(CommandType.NOLLAUS)
         )
 
         self._kumoa_painike = ttk.Button(
             master=self._root,
             text="Kumoa",
             state=constants.DISABLED,
-            command=lambda: self._suorita_komento(Komento.KUMOA)
+            command=lambda: self._suorita_komento(CommandType.KUMOA)
         )
 
         tulos_teksti.grid(columnspan=4)
@@ -55,23 +98,13 @@ class Kayttoliittyma:
         self._kumoa_painike.grid(row=2, column=3)
 
     def _suorita_komento(self, komento):
-        arvo = 0
+        komento_olio = self._komennot[komento]
+        komento_olio.suorita()
 
-        try:
-            arvo = int(self._syote_kentta.get())
-        except Exception:
-            pass
-
-        if komento == Komento.SUMMA:
-            self._sovelluslogiikka.plus(arvo)
-        elif komento == Komento.EROTUS:
-            self._sovelluslogiikka.miinus(arvo)
-        elif komento == Komento.NOLLAUS:
-            self._sovelluslogiikka.nollaa()
-        elif komento == Komento.KUMOA:
-            pass
-
-        self._kumoa_painike["state"] = constants.NORMAL
+        if self._sovelluslogiikka.edelliset_arvot_pituus() > 1:
+            self._kumoa_painike["state"] = constants.NORMAL
+        else:
+            self._kumoa_painike["state"] = constants.DISABLED
 
         if self._sovelluslogiikka.arvo() == 0:
             self._nollaus_painike["state"] = constants.DISABLED
--- viikko5/laskin/src/sovelluslogiikka.py
+++ viikko5/laskin/src/sovelluslogiikka.py
@@ -1,18 +1,28 @@
 class Sovelluslogiikka:
     def __init__(self, arvo=0):
         self._arvo = arvo
+        self._edelliset_arvot = [arvo]
 
     def miinus(self, operandi):
         self._arvo = self._arvo - operandi
+        self._edelliset_arvot.append(self._arvo)
 
     def plus(self, operandi):
         self._arvo = self._arvo + operandi
+        self._edelliset_arvot.append(self._arvo)
 
     def nollaa(self):
         self._arvo = 0
+        self._edelliset_arvot.append(self._arvo)
 
-    def aseta_arvo(self, arvo):
-        self._arvo = arvo
+    def kumoa(self):
+        if len(self._edelliset_arvot) < 2:
+            return
+        self._arvo = self._edelliset_arvot[-2]
+        self._edelliset_arvot.pop()
+
+    def edelliset_arvot_pituus(self):
+        return len(self._edelliset_arvot)
 
     def arvo(self):
         return self._arvo
